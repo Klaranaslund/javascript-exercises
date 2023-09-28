41161e18c2506fc7803fb9bc1ee82bd86d0afab7
Klaranaslund <naslundklara@gmail.com>
2023-09-28 13:21:52 +0200

    passed all javascript exercises for fundementals 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const myArr = string.split("");
+    const newStringArray = [];
+
+    let count = -1;
+    for(let i=myArr.length-1; i>=0; i--){
+        count++;
+        newStringArray[count] = myArr[i];
+}
+
+let reversedString = "";
+for (let i = 0; i<newStringArray.length; i++){
+    reversedString+=newStringArray[i];
+}
+
+return reversedString;
+    
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...optionalArgs) {
+    // Create a new array to store the elements to keep
+    const newArray = [];
 
+    for (let i = 0; i < array.length; i++) {
+        if (!optionalArgs.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
