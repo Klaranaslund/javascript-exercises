47ddbac109373925c135a7e8933c970db8d8c1e0
Klaranaslund <naslundklara@gmail.com>
2023-10-17 11:13:14 +0200

    solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+const palindrome = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+return palindrome.split("").reverse().join("") == palindrome;
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
