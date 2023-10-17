0f89bb2c41e85487a73833e2d146661c20c8751d
Klaranaslund <naslundklara@gmail.com>
2023-10-17 10:39:45 +0200

    solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+/** observera 1 ist för 0 pga multiplikation och inte addition */
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  return Math.pow(number, power);
 };
 
-const factorial = function() {
+/** Går också att lösa med rekursion */
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let factorial = 1;
+  for (let i = num; i>0; i--){
+    factorial *= i;
+  }
 	
+  return factorial;
 };
 
 // Do not edit below this line
