3e8a7e884e180d006c1bc325a253555609a31abf
Klaranaslund <naslundklara@gmail.com>
2023-10-17 12:18:36 +0200

    solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    if (index === 0) return 0;
 
+    let firstPrev = 1;
+    let secondPrev = 0;
+    
+    /**pekare som flyttas fram för varje varv */
+    for (let i = 2; i <= index; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
 };
 
-// Do not edit below this line
 module.exports = fibonacci;
